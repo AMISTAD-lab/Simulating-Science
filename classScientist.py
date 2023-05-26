e3d98ab93d9e43ddc1d235c5ef4e04db4849e57f
Marina <mkazarian@g.hmc.edu>
2023-05-26 15:02:39 -0700

    fixed errors in sciQuery

--- classScientist.py
+++ classScientist.py
@@ -17,15 +17,15 @@ class Scientist():
         # if they follow the herd
         for x in board.discovered:
             # find max payoff from discovered stuff
-            discoveredPayoffs.update({board.board[x[0]][x[1]].payoff: board.board[x[0]][x[1]]})
-        if choice == 1:
-            maxPayoff = max(list(discoveredPayoffs.keys()))
-            discoveredPayoffs.get(maxPayoff).cellQuery(board)
-            self.hindex += discoveredPayoffs.get(maxPayoff).cellQuery(board)
+            discoveredPayoffs.update({board.board[x[0]][x[1]].payoff : board.board[x[0]][x[1]]})
+        if choice == 1 and len(list(discoveredPayoffs.keys())) > 0:
+                maxPayoff = max(list(discoveredPayoffs.keys()))
+                discoveredPayoffs.get(maxPayoff).cellQuery(board)
+                self.hindex += discoveredPayoffs.get(maxPayoff).cellQuery(board)
         else:
             # random behavior from scientists
             x = np.random.randint(0, board.rows)
             y = np.random.randint(0, board.cols)
             board.board[x][y].cellQuery(board)
             self.hindex += board.board[x][y].cellQuery(board)
-        return [self.hindex, board.discovered, discoveredPayoffs, maxPayoff, discoveredPayoffs.get(maxPayoff)]
+        return self.hindex
