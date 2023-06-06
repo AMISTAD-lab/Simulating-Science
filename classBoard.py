1e20a4e12291ac8311ebb34b624ddbded22fcd63
Marina <mkazarian@g.hmc.edu>
2023-06-06 09:40:27 -0700

    updated heatmap

--- classBoard.py
+++ classBoard.py
@@ -14,41 +14,53 @@ class Board():
         self.randomRange = [0 for i in range(rangeVal)]
         [self.randomRange.append(x) for x in range(1, rangeVal+1)]
         # board has random payoff values in each cell, with more bias toward 0
-        self.board = [[Cell(np.random.choice(self.randomRange), (i, j)) for i in range(self.rows)] for j in range(self.cols)]
+        self.board = [[Cell(np.random.choice(self.randomRange), (i, j)) for j in range(self.cols)] for i in range(self.rows)]
         self.discovered = []
-        self.undiscovered = [self.board[i][j].location for i in range(self.rows) for j in range(self.cols)]
+        self.undiscovered = [self.board[i][j].location for j in range(self.cols) for i in range(self.rows)]
+        self.originalPays = self.getPayoffs()
 
     def __repr__(self):
         """string representation of Board"""
         return str(self.board)
 
     def getPayoffs(self):
-        """returns the payoff value for each cell of the board to make it easier
-        to get a 2D matrix of data"""
-        return [[self.board[i][j].payoff for i in range(self.rows)] for j in range(self.cols)]
+        """returns the payoff value for each cell of the board in a way that 
+        makes it easier to get a heatmap from a 2D matrix of data"""
+        return [[self.board[i][j].payoff for i in range(self.cols)] for j in range(self.rows)]
 
     def drawBoard(self, cellsHit, numRun):
         """produces a plot of the board for a given run and saves the image"""
         data = self.getPayoffs()
-        hm = sn.heatmap(data = data, vmin = 0, vmax = 30, cmap="Blues")
-        
+        plot = plt.imshow(data, cmap="Greens", vmin=0, vmax=30)
+        plt.colorbar()
+        # plt.imshow(self.originalPays, cmap="RdYlGn", vmin=0, vmax=30)
+
         for i in range(self.rows):
             for j in range(self.cols):
                 cell = self.board[i][j]
-                #finding the number of scientists on each cell each round
+                # finding the number of scientists on each cell each round
                 numSciHits = 0
                 if cell.location in cellsHit.keys():
                     numSciHits = len(cellsHit[cell.location])
                     # plotting the "dot" scientists
                     for x in range(1, numSciHits + 1):
-                        hm.plot(i+(x/(1 + numSciHits)), j + 0.55,
-                            marker="o", markersize=10, markeredgecolor="red",
-                            markerfacecolor="red")
-                #annotating cells (phase, numHits)
-                #(ex: B5 means breakthrough phase, hit 5 times)
-                hm.annotate(f"{cell.phase.name[0]}{cell.numHits}", xy = (j, i), xytext=(j+0.40, i+0.2),
-                    fontsize=8, fontweight='bold')
-        
+                        scientist = cellsHit[cell.location][x-1]
+                        starFactor = (scientist.hindex-(31 - scientist.career))/(31 - scientist.career)
+                        if starFactor < 0:
+                            dotSize = 5 - 5 * starFactor
+                        else:
+                            dotSize = 5 + 5 * starFactor
+                        # generate random locations within the cell for the scientist
+                        # offset of 0.5 from the way heatmap is generated
+                        randX = np.random.uniform(0.1, 0.9)
+                        randY = np.random.uniform(0.1, 0.9)
+                        plt.plot(i+randX-0.5, j+randY-0.5,
+                            marker="o", markersize=dotSize, markeredgecolor="blue",
+                            markerfacecolor="blue")
+                # annotating cells (phase, numHits)
+                # (ex: B5 means breakthrough phase, hit 5 times)
+                # add this for debugging payoffs: {f'{cell.payoff:.1f}'}
+                plt.annotate(f"{cell.phase.name[0]}{cell.numHits}", xy = (i, j), xytext=(i-0.15, j+0.075),
+                    fontsize=13, fontweight='bold')
         plt.savefig(f'testplot{numRun}.png')
         plt.clf()
-        #plt.show()
