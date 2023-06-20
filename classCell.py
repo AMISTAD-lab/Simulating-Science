e36f92fe272165c30e93d6de38b596d75cdcb013
Lydia Yang <lydyang@hmc.edu>
2023-06-20 15:55:42 -0700

    made stepSize variable for each query
    Co-authored-by: Marina Kazarian <marinakazarian@users.noreply.github.com>
    Co-authored-by: Alejandro Valdez <Alejandro-Valdez-Medina@users.noreply.github.com>

--- classCell.py
+++ classCell.py
@@ -9,14 +9,15 @@ class Cell():
         self.hidden = True
         self.phase = Phase.experimental
         self.numHits = 0
-        self.lowerx, self.upperx, self.slopeVals = self.logiFunc(15, 10, 0.5)
+        self.lowerx, self.upperx, self.yList = self.logiFunc(15, 10)
+        self.stepSize, self.slopeVals = self.setStepSize(10, 0.5)
         self.numSciHits = 0
 
     def __repr__(self):
         "string representation of Cell"
         return str(self.payoff)
 
-    def logiFunc(self, N, D, p):
+    def logiFunc(self, N, D):
         lowSuccess = False
         highSuccess  = False
 
@@ -26,7 +27,6 @@ class Cell():
             center = np.random.randint(0, 15)
             lowerx = 0
             upperx = []
-            slopeVals = {}
             lowSuccess = False
             highSuccess = False
             for x in np.arange(0, 50, 0.01):
@@ -38,49 +38,42 @@ class Cell():
                     upperx.append(math.ceil(x))
                     highSuccess = True
             
-        xmax = min(upperx)
-        #finds the y value (proportion) at given x in the logistic function
-        self.incHit = float(1/(1 + math.pow(math.e, -steepness*(xmax-center))))
-        self.breakHit = float(1/(1 + math.pow(math.e, -steepness*(lowerx-center))))
+            xmax = min(upperx)
+            #Goal: to get from x=0 to x=xmax in ksteps
+            # - Chop interval into N pieces
+            yList = []
+            interval = xmax/N
+            for x in np.arange(0, xmax, interval/D):
+                yval = float(1/(1 + math.pow(math.e, -steepness*(x-center))))
+                yList += [yval]
+        return lowerx, xmax, yList
 
-        #Goal: to get from x=0 to x=xmax in ksteps
-        # - Chop interval into N pieces
-        yList = []
-        interval = xmax/N
-        for x in np.arange(0, xmax, interval/D):
-            yval = float(1/(1 + math.pow(math.e, -steepness*(x-center))))
-            yList += [yval]
+    def setStepSize(self, D, p):
+        slopeVals = {}
         # - for Scientist at time i, flip coin (with probability p) D times (number of microsteps)
         stepSize = np.random.binomial(D*2, p, size=None)
+
+        print("location: ", self.location)
+        print("stepSize: ", stepSize)
 
         #putting the slopes in a dictionary
         prevY = 0
         i = 0
-        while i <= xmax: 
+        while i <= self.upperx: 
             if stepSize == 0:
                 slope = 0
             else:
-                slope = (yList[i+stepSize] - prevY)/stepSize
+                slope = (self.yList[i+stepSize] - prevY)/stepSize
             slopeVals[i] = slope
-            prevY = yList[i+stepSize]
+            prevY = self.yList[i+stepSize]
             i += 1
-        return lowerx, xmax, slopeVals
+        return stepSize, slopeVals
 
     def cellQuery(self, board):
         """returns payoff for each scientist based on conditions of a cell"""
         original = self.payoff
         sciPayoff = 0.0
-
-        originalPay = board.originalPays[self.location[1]][self.location[0]]
-        # if self.payoff/originalPay >= self.incHit:
-        #     self.phase = Phase.incremental
-        # elif self.payoff/originalPay >= self.breakHit:
-        #     self.phase = Phase.breakthrough
-        #     self.hidden = False
-        #     # "discover" the cell once it hits breakthrough phase
-        #     if self.location not in board.discovered:
-        #         board.discovered.append(self.location)
-        #         board.undiscovered.remove(self.location)
+        self.setStepSize(10, 0.5)
 
         if self.numHits > self.upperx:
             self.phase = Phase.incremental
